3a8b15d1d9a3411c5f0bd189ab790cec6482303c
shemoks <shemshur@ukr.net>
2015-11-08 01:04:07 +0200

    created class and test for it

---
+++ app/models/MyClass.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oksana
+ * Date: 06.11.15
+ * Time: 23:05
+ */
+
+namespace MyClass;
+
+
+class MyClass
+{
+    public function power($x, $y)
+    {
+        return pow($x, $y);
+    }
+
+}
